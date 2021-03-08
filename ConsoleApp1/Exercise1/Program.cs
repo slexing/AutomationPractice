9b00f9cb09caac17a2d57ada21681ce3cb7a0d59
OlegB <o.bratchikov@yandex.ru>
2021-03-08 15:40:28 +0300

    test

---
+++ ConsoleApp1/Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your x number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your y number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your z number:");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            int first = (x + y) * (z * z + 1);
+
+            double second = (x % z - 1) * Math.Sqrt(y);
+
+            double third = ((x * y) + (y * z)) / z * z * z;
+
+            Console.WriteLine($"First expression result: {first}");
+            Console.WriteLine($"Second expression result: {second}");
+            Console.WriteLine($"Third expression result: {third}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Exercise4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую строку");
+            string first = Console.ReadLine();
+            Console.WriteLine("Введите вторую строку");
+            string second = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            char symbol = Console.ReadKey().KeyChar;
+            Console.WriteLine("");
+            var sum = first +" "+second;
+            var index = sum.LastIndexOf(symbol);
+
+            Console.WriteLine($"Concatenation string is {sum}");
+            Console.WriteLine($"Index of {symbol} on {sum}:{index}");
+            Console.ReadKey();
+
+        }
+    }
+}
